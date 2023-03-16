7c22e96d87e0a027c3c50cf9cf54dd136a7d56d3
Admin O <olexandro@gmail.com>
2023-03-17 01:24:55 +0300

    Шаблон

--- src/main/java/com/skypro/courseproject_4_telegram_bot/CourseProject4TelegramBotApplication.java
+++ src/main/java/com/skypro/courseproject_4_telegram_bot/CourseProject4TelegramBotApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseProject4TelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseProject4TelegramBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProject4TelegramBotApplication.class, args);
+    }
 
 }
