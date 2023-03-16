888e0bacff8de1581a49177f91ddeb282ac261dd
Admin O <olexandro@gmail.com>
2023-03-17 00:58:29 +0300

    Initial commit

---
+++ src/main/java/com/skypro/courseproject_4_telegram_bot/CourseProject4TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.courseproject_4_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProject4TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseProject4TelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/courseproject_4_telegram_bot/CourseProject4TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.courseproject_4_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject4TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
